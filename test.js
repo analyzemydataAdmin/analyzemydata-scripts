326e41361687c707f9572b0f21c9af2cfb54f416
analyzemydataAdmin <analyzemydata631@gmail.com>
2024-07-24 22:11:47 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+// script.js
+window.onload = function() {
+    // Find the HTML element with the id "message"
+    var messageElement = document.getElementById('message');
+    
+    // Change its content
+    messageElement.innerHTML = 'Hello, world!';
+};
